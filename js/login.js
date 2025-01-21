feb1ad1a471fd4f0f6442e45bc6b01d1aaf829cd
Andlie94 <andlie02174@stud.noroff.no>
2025-01-21 23:38:08 +0100

    Refactor login form and improve login functionality

--- js/login.js
+++ js/login.js
@@ -6,25 +6,41 @@ async function handelLogin(event) {
 
     try {
         const response = await fetch("https://v2.api.noroff.dev/auth/login", {
-            method: "POST",
+            method: "POST", 
             headers: {
                 "Content-Type": "application/json",
+                "accept": "application/json"
             },
             body: JSON.stringify({ email, password }),
         });
 
         if (!response.ok) {
-            alert("inn loggning feilet Sjekk om epost og passord er riktig");
-            throw new Error("inn loggning feilet");
+            alert("Innlogging feilet. Sjekk om e-post og passord er riktig.");
+            throw new Error("Innlogging feilet");
         }
 
         const data = await response.json();
-        const token = data.data.accessToken;
-        const userEmail = data.data.email;
+        const token = data.accessToken; 
+        const userEmail = data.email;
         localStorage.setItem("token", token);
         localStorage.setItem("userEmail", userEmail);
-        window.location.href = "edit.html";
+        
+        window.location.href = "register.html";
+        console.log(response);
     } catch (error) {
-        console.error("Wrong password or email", error);
+        console.error("Feil e-post eller passord", error);
     }
 }
+function isLoggedIn() {
+    const token = localStorage.getItem("token");
+    return token !== null; 
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (isLoggedIn()) {
+        const email = localStorage.getItem("brukerEmail");
+        console.log(`Logget inn som: ${email}`);
+    } else {
+        console.log("Brukeren er ikke logget inn");
+    }
+});
