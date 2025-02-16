f21bde5d01ef2dcbd082d712bea4eb9c2a1b425e
Andlie94 <andlie02174@stud.noroff.no>
2025-02-16 18:27:00 +0100

    Fikset fetch på login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,6 @@
-export const X_NOROFF_API_KEY = "580b33a9-04f3-4da3-bb38-de9adcf9d9f8";
+
+
+import { login } from './api.js';  
 
 document.getElementById("loginForm").addEventListener("submit", async function (event) {
     event.preventDefault();
@@ -9,33 +11,22 @@ document.getElementById("loginForm").addEventListener("submit", async function (
     const userData = { email, password };
 
     try {
-        const response = await fetch("https://v2.api.noroff.dev/auth/login", {
-            method: "POST",
-            headers: { 
-                "Content-Type": "application/json",
-                "X-Noroff-API-Key": X_NOROFF_API_KEY
-            },
-            body: JSON.stringify(userData)
-        });
 
-        const result = await response.json();
+        const result = await login(userData);
+
         console.log("Full API Response:", result); 
 
-        if (response.ok) {
-            if (!result.data) {
-                throw new Error("Data is undefined!");
-            }
-
-            const token = result.data.accessToken;
-            const userEmail = result.data.email;
-            const userName = result.data.name;
+        if (result) {
+            const token = result.accessToken;
+            const userEmail = result.email;
+            const userName = result.name;
 
             console.log("Token:", token);
             console.log("Email:", userEmail);
             console.log("Name:", userName);
 
             if (!token || !userEmail || !userName) {
-                throw new Error("Token, email or navn is undefined!");
+                throw new Error("Token, email or name is undefined!");
             }
 
             localStorage.setItem("token", token);
@@ -47,14 +38,12 @@ document.getElementById("loginForm").addEventListener("submit", async function (
             setTimeout(() => {
                 window.location.href = "../post/feedpage.html"; 
             }, 2000);
-        } else {
-            console.error("Feilrespons fra API:", result);
-            document.getElementById("message").textContent = "Error: " + (result.errors ? result.errors[0].message : "Unknown error");
         }
     } catch (error) {
-        console.error("Innloggingsfeil:", error);
-        document.getElementById("message").textContent = "something went wrong. Please try again.";
+        console.error("Login failed:", error);
+        document.getElementById("message").textContent = "Something went wrong. Please try again.";
     }
 });
+
 
 
